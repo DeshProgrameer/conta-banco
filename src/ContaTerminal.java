d67274b6154e8e339df2c0ba33e5b42b859f6cff
DeshCoder <Evandrosune@gmail.com>
2023-09-27 11:31:52 -0300

    Projeto conta concluído

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TODO:conhecer e importar a classe Scanner
+
+        // Exibir as mensagens para o nosso usuário
+        // Obter pela scanner os valores digitados no terminal
+        //Exibir a mensagen conta criada
+
+        Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Por favor, digite o nome do Titular da conta ! ");
+        String nomeTitular = scanner.nextLine();
+
+                System.out.println("Por favor, digite o número da Conta ! ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência ! ");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Por favor, digite o saldo da conta ! ");
+        double saldo = scanner.nextDouble();
+
+        ContaTerminal conta = new ContaTerminal();
+
+        System.out.println("Olá, " + nomeTitular + " obrigado por criar uma conta conosco, sua agência é " + agencia + "  conta  " + numero + " e seu saldo de " + saldo + " já está disponível para saque. ");
     }
 }
